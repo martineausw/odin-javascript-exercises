621d070009907e33cfeb905f35fb033873b70853
Seth Martineau <martineausw@gmail.com>
2025-12-18 15:39:23 -0500

    Complete foundations/09_sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof(a) != "number" || typeof(b) != "number")
+        return "ERROR";
 
+    if (a < 0 || b < 0) 
+        return "ERROR";
+        
+    if (a % 1 !== 0 || b % 1 !== 0)
+        return "ERROR";
+
+    const max = Math.max(a, b);
+    const min = Math.min(a, b);
+    return max * (max + 1) / 2 - min + 1;
 };
 
 // Do not edit below this line
