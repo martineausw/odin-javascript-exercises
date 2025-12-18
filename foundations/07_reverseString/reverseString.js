e6034056c6361e3464533088096f0120e8f52505
Seth Martineau <martineausw@gmail.com>
2025-12-18 15:18:32 -0500

    Complete foundations/07_reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+    for (let i = 1; i <= string.length; i++) newString += string.at(-i);
+    return newString;
 };
 
 // Do not edit below this line
