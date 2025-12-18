778ca8f8dcdf09f2b6dd569d82cc1297c8a9276d
Seth Martineau <martineausw@gmail.com>
2025-12-18 15:26:13 -0500

    Complete foundations/08_removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    let newArray = array;
+    for (const element of elements) 
+        newArray = newArray.filter(item => item !== element);
+    return newArray;
 };
 
 // Do not edit below this line
